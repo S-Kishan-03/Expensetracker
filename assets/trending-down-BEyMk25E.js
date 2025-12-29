017e116564c4cdd80d06c2c50de326d7c49207ed
S-Kishan-03 <s.kishan9603@gmail.com>
2025-12-29 15:19:47 +0530

    Updates

---
+++ assets/trending-down-BEyMk25E.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-CR9WIwuk.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["rect",{width:"20",height:"14",x:"2",y:"5",rx:"2",key:"ynyp8z"}],["line",{x1:"2",x2:"22",y1:"10",y2:"10",key:"1b3vmo"}]],t=e("credit-card",n);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["polyline",{points:"22 17 13.5 8.5 8.5 13.5 2 7",key:"1r2t7k"}],["polyline",{points:"16 17 22 17 22 11",key:"11uiuu"}]],c=e("trending-down",o);export{t as C,c as T};
